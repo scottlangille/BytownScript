0fce0a009e3e430d31f5351f275a3c2fbccb4772
scottlangille <46726118+scottlangille@users.noreply.github.com>
2020-12-21 19:30:48 -0800

    Add userscript as update URL

---
+++ userscript.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Bytown Modern Sales Auto-Price
+// @namespace    modernsales.ca
+// @version      2.0
+// @description  Automatically calculates prices.
+// @author       Scott L.
+// @include      http://msc.cylink.ca/memberconnect/QuickOrderProcessing.do*
+// @include      https://msc.cylink.ca/memberconnect/QuickOrderProcessing.do*
+// @resource csv file://bytown_rxserv/prxrem/Shared/scott/product_group_pricing.csv
+// @grant        GM_getResourceText
+// @require      https://raw.githubusercontent.com/scottlangille/BytownScript/main/script.js
+// ==/UserScript==
